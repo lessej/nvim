d2475bf3eeff05e5d87d65a0a17e4a7650cf38d0
Jesse Leung <jessetleung@aol.com>
2023-11-01 23:33:19 +0900

    Add keymaps for executor

--- after/plugin/executor.lua
+++ after/plugin/executor.lua
@@ -1,5 +1,6 @@
 require('executor').setup({})
 
 vim.keymap.set('n', '<leader>xr', ':ExecutorRun<CR>')
+vim.keymap.set('n', '<leader>xc', ':ExecutorSetCommand<CR>')
 vim.keymap.set('n', '<leader>xd', ':ExecutorToggleDetail<CR>')
 vim.keymap.set('n', '<leader>xh', ':ExecutorShowHistory<CR>')
