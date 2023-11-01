4d80ab3e997332e01391eb445b96d76b057ee6a6
Jesse Leung <jessetleung@aol.com>
2023-11-01 22:49:39 +0900

    add remaps for executor

--- after/plugin/executor.lua
+++ after/plugin/executor.lua
@@ -1,1 +1,5 @@
 require('executor').setup({})
+
+vim.keymap.set('n', '<leader>xr', ':ExecutorRun<CR>')
+vim.keymap.set('n', '<leader>xd', ':ExecutorToggleDetail<CR>')
+vim.keymap.set('n', '<leader>xh', ':ExecutorShowHistory<CR>')
